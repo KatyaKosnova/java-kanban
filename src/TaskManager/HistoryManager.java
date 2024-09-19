d234e4e8f8712f1e8cdcdf81fda7565dc78b857d
KatyaKosnova <kosnovakaty@gmail.com>
2024-09-19 21:34:28 +0500

    .

--- src/TaskManager/HistoryManager.java
+++ src/TaskManager/HistoryManager.java
@@ -6,6 +6,8 @@ import java.util.List;
 public interface HistoryManager {
 
     void add(Task task);          // Добавление задачи в историю
+
     void remove(int id);          // Удаление задачи по ID
+
     List<Task> getHistory();      // Получение истории просмотров
 }
