fc9ab41591e3dd3a3f0b7e6c9b80c036ca2cd1b3
KatyaKosnova <kosnovakaty@gmail.com>
2024-09-19 21:43:05 +0500

    .

--- src/Task/Epic.java
+++ src/Task/Epic.java
@@ -1,4 +1,5 @@
 package task;
+
 import taskstatus.TaskStatus;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,27 +9,23 @@ public class Epic extends Task {
     private List<Subtask> subtasks;
 
     public Epic(int id, String name, String description, TaskStatus status) {
-
         super(id, name, description, status);
         this.subtasks = new ArrayList<>();
     }
 
     public List<Subtask> getSubtasks() {
-
         return subtasks;
     }
 
     public void addSubtask(Subtask subtask) {
-
         subtasks.add(subtask);
     }
 
     public void removeSubtask(Subtask subtask) {
-
         subtasks.remove(subtask);
     }
-    public void updateSubtask(Subtask subtask) {
 
+    public void updateSubtask(Subtask subtask) {
         for (int i = 0; i < subtasks.size(); i++) {
             if (subtasks.get(i).getId() == subtask.getId()) {
                 subtasks.set(i, subtask);
@@ -37,4 +34,3 @@ public class Epic extends Task {
         }
     }
 }
-
